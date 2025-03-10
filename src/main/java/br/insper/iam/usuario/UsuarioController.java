9d7c81351983dac05fd177750c8f0cacea6f567b
luabatepietro <luabatepietroapple@icloud.com>
2025-03-10 15:11:18 -0300

    fix

--- src/main/java/br/insper/iam/usuario/UsuarioController.java
+++ src/main/java/br/insper/iam/usuario/UsuarioController.java
@@ -1,3 +1,10 @@
+package br.insper.iam.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/usuario")
 public class UsuarioController {
@@ -11,4 +18,17 @@ public class UsuarioController {
     }
 
     @GetMapping
-    public
+    public List<Usuario> getUsuarios() {
+        return usuarioService.getUsuarios();
+    }
+
+    @GetMapping("/{email}")
+    public Usuario getUsuario(@PathVariable String email) {
+        return usuarioService.findUsuarioByEmail(email);
+    }
+
+    @PostMapping
+    public Usuario saveUsuario(@RequestBody Usuario usuario) {
+        return usuarioService.saveUsuario(usuario);
+    }
+}
